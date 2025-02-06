fff84fabab1db01c5134f58bd9d65923cd9c0be6
Jan B. <j.bruchwalski@web.de>
2025-02-07 00:27:17 +0100

    started with registry component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RegistryComponent } from './registry/registry.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RegistryComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
