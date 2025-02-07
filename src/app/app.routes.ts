73a72bc3222f13082457327bd99dd46cae0d856e
schmax <majorsoldier603@proton.me>
2025-02-07 09:50:03 +0100

    add basic file structure + add basic routes + angular fire and services work in progress

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { RegistryComponent } from "./registry/registry.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '/', component: RegistryComponent },
+];
