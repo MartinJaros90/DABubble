3b63f01b9bc2aa4c804c8bc18af44e96780e89a0
schmax <majorsoldier603@proton.me>
2025-02-09 17:07:07 +0100

    add auth guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
+import { AuthGuard, redirectUnauthorizedTo } from "@angular/fire/auth-guard";
 import { LogonComponent } from './logon-component/logon.component';
 import { SignUpComponent } from "./logon-component/childs/sign-up/sign-up.component";
 import { SignInComponent } from './logon-component/childs/sign-in/sign-in.component';
+import { MainContentComponent } from './main-content/main-content.component';
 
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['logon']);
+// basic auth guard wenn ihr eine neuen router link macht bitte die auth guard config anpassen oder schreibt mir @max
 export const routes: Routes = [
+    { path: '', component: MainContentComponent, canActivate: [AuthGuard], data: { authGuardPipe: redirectUnauthorizedToLogin } },
     {
         path: 'logon',
         component: LogonComponent,
