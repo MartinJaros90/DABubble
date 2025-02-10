c04965ae9eee1312c5c8bb312ed7a7b505e70cf4
Lukasz Szymczak <lukas.sz.br@gmail.com>
2025-02-11 00:18:56 +0100

    my profile card - finish

--- src/app/lukas-components/my-profile-card/my-profile-card.component.ts
+++ src/app/lukas-components/my-profile-card/my-profile-card.component.ts
@@ -1,32 +1,87 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-my-profile-card',
-  imports: [CommonModule, MatFormFieldModule, MatInputModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './my-profile-card.component.html',
   styleUrl: './my-profile-card.component.scss'
 })
 
 export class MyProfileCardComponent {
 
+  // --------------------------------------------//
+
   profileData = {
-    avatar: 4,
+    avatar: 6,
     name: 'Frederik Beck',
     email: 'mail@frederik-beck.com'
   }
 
+  // --------------------------------------------//
+
   avatarUrl = 'assets/lukas-icons/profile-card/avatar-' + this.profileData.avatar + '.svg';
-  edit = true;
+  edit = false;
+  editAvatar = false;
+  newName = '';
+  avatars: number[] = Array.from({ length: 7 }, (_, i) => i);
+  currentAvatar = this.profileData.avatar;
+
 
   ngOnInit() {
 
+    this.loadFirebase();
+  }
 
+  ngOnDestroy() {
 
   }
 
+  editCancel() {
+
+    if (this.currentAvatar != this.profileData.avatar) this.avatarChooseClick(this.currentAvatar);
+    this.editAvatar = false;
+    this.edit = false;
+    this.newName = ''; 
+  }
+
+  editSave() {
+
+    this.newName = this.newName.trim();
+    if (this.newName != "") {
+      this.profileData.name = this.newName;
+      this.newName = '';
+      this.saveFirebase();
+    }
+    this.edit = false;
+    this.editAvatar = false;
+  }
+
+  avatarChooseClick(index: number) {
+
+    this.profileData.avatar = index;
+    this.avatarUrl = 'assets/lukas-icons/profile-card/avatar-' + this.profileData.avatar + '.svg';
+  }
+
+  // ----------------------------------------------------------------------------------------------//
+  /**
+   * 
+   *   ----- bitte nichts änern an meinem code ohne absprache -----
+   * 
+   *   zu profleData beim load
+   * 
+   *   von profleData beim save
+   *  
+   */
+
+  loadFirebase() {
+
+  }
+
+  saveFirebase() {
+
+  }
 }
 
 
