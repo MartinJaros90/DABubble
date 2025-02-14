3f43c0512f17f70486c5570b9b903c6ae985ae85
Martin Jaros <martinjaros88@gmail.com>
2025-02-14 21:50:49 +0100

    feat: implement devspace container content

--- src/app/workspace/workspace.component.ts
+++ src/app/workspace/workspace.component.ts
@@ -4,12 +4,14 @@ import { Component } from '@angular/core';
   selector: 'app-workspace',
   imports: [],
   templateUrl: './workspace.component.html',
-  styleUrl: './workspace.component.scss'
+  styleUrl: './workspace.component.scss',
 })
 export class WorkspaceComponent {
-
   isThreadVisible = true;
   isDevspaceVisible = true;
+  isChannelsOpen = true;
+  isDirectMessagesOpen = true;
+  showAddChannel = false;
 
   toggleThread() {
     this.isThreadVisible = !this.isThreadVisible;
@@ -19,12 +21,35 @@ export class WorkspaceComponent {
     this.isDevspaceVisible = !this.isDevspaceVisible;
   }
 
+  toggleChannels() {
+    this.isChannelsOpen = !this.isChannelsOpen;
+  }
+
+  toggleDirectMessages() {
+    this.isDirectMessagesOpen = !this.isDirectMessagesOpen;
+  }
+
+  toggleAddChannel() {
+    this.showAddChannel = !this.showAddChannel;
+  }
+
   getChatClasses(): string {
     const classes = ['chat'];
     if (!this.isThreadVisible) classes.push('thread-hidden');
     if (!this.isDevspaceVisible) classes.push('devspace-hidden');
-    if (!this.isThreadVisible && !this.isDevspaceVisible) classes.push('both-hidden');
+    if (!this.isThreadVisible && !this.isDevspaceVisible)
+      classes.push('both-hidden');
     return classes.join(' ');
   }
 
+  openChannelDialog(event: Event) {
+    event.stopPropagation();
+    console.log('Channel Dialog öffnen');
+  }
+
+  getWorkspaceMenuText(): string {
+    return this.isDevspaceVisible
+      ? 'Workspace-Menü schließen'
+      : 'Workspace-Menü öffnen';
+  }
 }
