bc45ffef95f9c9271c60fc399f69778cd6cb322b
Martin Jaros <martinjaros88@gmail.com>
2025-02-17 20:33:29 +0100

    refactor: remove redundant standalone:true declarations

--- src/app/splash-screen/splash-screen.component.ts
+++ src/app/splash-screen/splash-screen.component.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-splash-screen',
-  standalone: true,
   imports: [CommonModule],
   templateUrl: './splash-screen.component.html',
   styleUrls: ['./splash-screen.component.scss'],
--- src/app/workspace/devspace/devspace.component.ts
+++ src/app/workspace/devspace/devspace.component.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-devspace',
-  standalone: true,
   imports: [CommonModule],
   templateUrl: './devspace.component.html',
   styleUrl: './devspace.component.scss',
