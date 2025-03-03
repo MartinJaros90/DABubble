b190c7496088288587840b4e054b85586ca3bb60
Martin Jaros <martinjaros88@gmail.com>
2025-03-03 22:30:45 +0100

    Performed refactoring on the emoji picker component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { PickerModule } from '@ctrl/ngx-emoji-mart';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, PickerModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  standalone: true
+  standalone: true,
 })
 export class AppComponent {
   showSplash = true;
